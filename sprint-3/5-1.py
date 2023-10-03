31c6f51c70ebda634f549a7328f5d9e58387ccd7
Vadim Kuznetsov <darovadraste@gmail.com>
2023-10-03 10:38:26 +0300

    sprint3 5-1 - 5-4 tasks

---
+++ sprint-3/5-1.py
@@ -0,0 +1,58 @@
+class Bird:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+
+    def describe(self, full=False):
+        return f'Размер птицы {self.name} — {self.size}.'
+
+
+class Parrot(Bird):
+    def __init__(self, name, size, color):
+        super().__init__(name, size)
+        self.color = color
+
+    def describe(self, full=False):
+        if full:
+            return (f'Попугай {self.name} — заметная птица, '
+                    f'окрас её перьев — {self.color}, '
+                    f'а размер — {self.size}. '
+                    'Интересный факт: попугаи чувствуют ритм, '
+                    'а вовсе не бездумно двигаются под музыку. '
+                    'Если сменить композицию, '
+                    'то и темп движений птицы изменится.')
+        return super().describe()
+
+    # Добавьте метод repeat().
+    def repeat(self, phrase):
+        return f'Попугай {self.name} говорит: {phrase}.'
+
+
+class Penguin(Bird):
+    def __init__(self, name, size, genus):
+        super().__init__(name, size)
+        self.genus = genus
+
+    def describe(self, full=False):
+        if full:
+            return (f'Размер пингвина {self.name} '
+                    f'из рода {self.genus} — {self.size}. '
+                    'Интересный факт: однажды группа геологов-разведчиков '
+                    'похитила пингвинье яйцо, '
+                    'и их принялась преследовать вся стая, '
+                    'не пытаясь, впрочем, при этом нападать. '
+                    'Посовещавшись, похитители вернули птицам яйцо, '
+                    'и те отстали. ')
+        return super().describe()
+
+    # Добавьте метод swimming().
+    def swimming(self):
+        return f'Пингвин {self.name} плавает со средней скоростью 11 км/ч.'
+
+
+kesha = Parrot('Ара', 'средний', 'красный')
+kowalski = Penguin('Королевский', 'большой', 'Aptenodytes')
+
+
+print(kesha.repeat('Кеша хороший!'))
+print(kowalski.swimming())

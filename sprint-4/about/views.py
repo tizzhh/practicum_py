1469cb8e57fd4eb3a190cda5d2a5cba608e0e956
Vadim Kuznetsov <darovadraste@gmail.com>
2023-10-20 15:07:08 +0300

    sprint-4 task2-2

--- sprint-4/about/views.py
+++ sprint-4/about/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def description(request):
-    return HttpResponse()
+    return HttpResponse('<h1>Урра, заработало!</h1>')
--- sprint-4/anfisa_for_friends/urls.py
+++ sprint-4/anfisa_for_friends/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
     path('', include('homepage.urls')),
+    path('about/', include('about.urls')),
+    path('admin/', admin.site.urls),
 ]

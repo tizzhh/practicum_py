a8f1bfb9bf6f6e17fef549a2ad91cdfec25ff2a7
Vadim Kuznetsov <darovadraste@gmail.com>
2023-10-21 12:37:32 +0300

    sprint-5 task5-3.3

--- sprint-4/anfisa_for_friends/settings.py
+++ sprint-4/anfisa_for_friends/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'anfisa_for_friends.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- sprint-4/ice_cream/views.py
+++ sprint-4/ice_cream/views.py
@@ -2,33 +2,34 @@ from django.shortcuts import render
 
 ice_cream_catalog = [
     {
-        "id": 0,
-        "title": "Классический пломбир",
-        "description": "Настоящее мороженое, "
-        "для истинных ценителей вкуса. "
-        "Если на столе появляется пломбир"
-        " — это не надолго.",
+        'id': 0,
+        'title': 'Классический пломбир',
+        'description': 'Настоящее мороженое, '
+                       'для истинных ценителей вкуса. '
+                       'Если на столе появляется пломбир'
+                       ' — это не надолго.',
     },
     {
-        "id": 1,
-        "title": "Мороженое с кузнечиками",
-        "description": "В колумбийском стиле: мороженое "
-        "с добавлением настоящих карамелизованных кузнечиков.",
+        'id': 1,
+        'title': 'Мороженое с кузнечиками',
+        'description': 'В колумбийском стиле: мороженое '
+                       'с добавлением настоящих карамелизованных кузнечиков.',
     },
     {
-        "id": 2,
-        "title": "Мороженое со вкусом сыра чеддер",
-        "description": "Вкус настоящего сыра в вафельном стаканчике.",
+        'id': 2,
+        'title': 'Мороженое со вкусом сыра чеддер',
+        'description': 'Вкус настоящего сыра в вафельном стаканчике.',
     },
 ]
 
 
 def ice_cream_detail(request, pk):
-    template = "ice_cream/detail.html"
-
-    return render(request, template, {"ice_cream": ice_cream_catalog[pk]})
+    template = 'ice_cream/detail.html'
+    context = {'ice_cream': ice_cream_catalog[pk]}
+    return render(request, template, context)
 
 
 def ice_cream_list(request):
-    template = "ice_cream/list.html"
-    return render(request, template)
+    template = 'ice_cream/list.html'
+    context = {'ice_cream_catalog': ice_cream_catalog}
+    return render(request, template, context)

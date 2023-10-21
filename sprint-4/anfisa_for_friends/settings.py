af4f626771b81fa78b99242ee73e0147f0922b8a
Vadim Kuznetsov <darovadraste@gmail.com>
2023-10-21 12:20:56 +0300

    sprint-5 task5-3.2

--- sprint-4/anfisa_for_friends/settings.py
+++ sprint-4/anfisa_for_friends/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ WSGI_APPLICATION = 'anfisa_for_friends.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- sprint-4/anfisa_for_friends/urls.py
+++ sprint-4/anfisa_for_friends/urls.py
@@ -1,13 +1,9 @@
-
 from django.contrib import admin
 from django.urls import include, path
-
-from anfisa_for_friends.views import test_rendering
 
 urlpatterns = [
     path('', include('homepage.urls')),
     path('about/', include('about.urls')),
     path('ice_cream/', include('ice_cream.urls')),
     path('admin/', admin.site.urls),
-    path('test_rendering/', test_rendering),
 ]

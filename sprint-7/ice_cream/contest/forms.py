1f112678495d2447fdbac12eb0ef7af35df2c18b
Vadim Kuznetsov <darovadraste@gmail.com>
2023-11-24 15:57:43 +0300

    sprint-7 lesson-2

--- sprint-7/ice_cream/contest/forms.py
+++ sprint-7/ice_cream/contest/forms.py
@@ -1,1 +1,8 @@
-# Опишите класс формы здесь!
+from django import forms
+
+
+class ContestForm(forms.Form):
+    title = forms.CharField(label='Название', max_length=20)
+    description = forms.CharField(label='Описание', widget=forms.Textarea())
+    price = forms.IntegerField(label='Цена', min_value=10, max_value=100, help_text='Рекомендованная розничная цена')
+    comment = forms.CharField(label='Комментарий', widget=forms.Textarea(), required=False)
--- sprint-7/ice_cream/contest/views.py
+++ sprint-7/ice_cream/contest/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .forms import ContestForm
 
 
 def proposal_create(request):
-    return render(request, 'contest/form.html')
+    return render(request, 'contest/form.html', context={'form': ContestForm()})
 
 
 def accepted(request):

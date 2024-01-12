68cc3ab65dde480a21ee1b864752a20ed58b41d1
Vadim Kuznetsov <darovadraste@gmail.com>
2024-01-12 23:09:34 +0300

    theme 1 lesson 6

---
+++ sprint-10/swapi.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+
+import requests
+
+
+response = requests.get('https://swapi.dev/api/people/?search=luke').json()['results'][0]
+diameter = requests.get(response['homeworld']).json()['diameter']
+
+print(diameter)
